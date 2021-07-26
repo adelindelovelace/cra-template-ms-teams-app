28a38f7689e4bc21c478d930f7ad18e63ef5e299
Adelinde Lovelace <85863019+adelindelovelace@users.noreply.github.com>
2021-07-26 09:38:40 +0800

    Update homepage.tsx

--- MS-Teams-React-App-Web-App/src/components/homepage/homepage.tsx
+++ MS-Teams-React-App-Web-App/src/components/homepage/homepage.tsx
@@ -16,18 +16,18 @@ import InitialDisplayPage from "../initial-display-page/initial-display-page";
 import AboutPage from "../about-page/about-page";
 
 import * as microsoftTeams from "@microsoft/teams-js";
-import { 
-  loadMsTeamsToLocalStorage, 
+import {
+  loadMsTeamsToLocalStorage,
   addObjectToLocalStorage,
   StorageNames,
-  getObjectFromLocalStorage
+  getObjectFromLocalStorage,
 } from "../../shared/helpers/local-storage-helper";
 
 interface IHomepageState {
-  context?: microsoftTeams.Context
+  context?: microsoftTeams.Context;
 }
 
-class Homepage extends React.Component<{}, any> {
+class Homepage extends React.Component<any, any> {
   state: IHomepageState;
 
   constructor(props: any) {
@@ -46,25 +46,31 @@ class Homepage extends React.Component<{}, any> {
   componentDidMount() {
     microsoftTeams.initialize();
     microsoftTeams.getContext((context: microsoftTeams.Context) => {
-      addObjectToLocalStorage(context, StorageNames.Context);  
-      
-      // TO ADD: 
+      this.setState({ context: context });
+      addObjectToLocalStorage(context, StorageNames.Context);
+
+      // TO ADD:
       // additional files for initial retrieval to local storage or variables
     });
   }
 
   render() {
     let msEntityId = this.state?.context?.entityId;
-    if (msEntityId === "MS Teams React App" || msEntityId === undefined) {
+    if (msEntityId === "MS Teams React App") {
       return (
         <div>
           <main>
             <div className="navigation">
-              <img src="favicon.ico" height="40"/>
-              <h5 className="title nav-link"><a onClick={() => this.page("home")}> MS Teams React App</a></h5>
-              <span className="nav-link"><a onClick={() =>this.page("home")}> Home</a></span>
-              <span className="nav-link"><a onClick={() =>this.page("about")}> About</a></span>
-              
+              <img src="favicon.ico" height="40" />
+              <h5 className="title nav-link">
+                <a onClick={() => this.page("home")}> MS Teams React App</a>
+              </h5>
+              <span className="nav-link">
+                <a onClick={() => this.page("home")}> Home</a>
+              </span>
+              <span className="nav-link">
+                <a onClick={() => this.page("about")}> About</a>
+              </span>
             </div>
             <Switch>
               {
@@ -73,14 +79,21 @@ class Homepage extends React.Component<{}, any> {
               <Route exact path="/home" component={InitialDisplayPage} />
               <Route exact path="/about" component={AboutPage} />
               <Route exact path="/" component={InitialDisplayPage} />
-              <Route path="/config" component={TabConfig} />
 
               {
                 // Sign-in pages and error pages
               }
               <Route path="/signin" component={SignInPage} />
-              <Route exact path="/signin-simple-start" component={SignInSimpleStart} />
-              <Route exact path="/signin-simple-end" component={SignInSimpleEnd} />
+              <Route
+                exact
+                path="/signin-simple-start"
+                component={SignInSimpleStart}
+              />
+              <Route
+                exact
+                path="/signin-simple-end"
+                component={SignInSimpleEnd}
+              />
               <Route exact path="/errorpage" component={ErrorPage} />
               <Route exact path="/errorpage/:id" component={ErrorPage} />
             </Switch>
@@ -88,7 +101,7 @@ class Homepage extends React.Component<{}, any> {
         </div>
       );
     } else {
-      return ( 
+      return (
         <>
           {
             //On Config
